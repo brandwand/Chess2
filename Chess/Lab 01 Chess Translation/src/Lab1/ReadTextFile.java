b49f053c2d8cc25977cb4d98b7e0f29418de8f01
brandwand <brandwand@yahoo.com>
2016-07-29 17:08:25 -0600

    Almost have the side by side entries done.

--- Chess/Lab 01 Chess Translation/src/Lab1/ReadTextFile.java
+++ Chess/Lab 01 Chess Translation/src/Lab1/ReadTextFile.java
@@ -1,7 +1,6 @@
 package Lab1;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.regex.Matcher;
@@ -12,7 +11,6 @@ public class ReadTextFile {
 	private BufferedReader read;
 	private String line;
 
-
 	public ReadTextFile(InputStream fileName) {
 		try {
 			read = new BufferedReader(new InputStreamReader(fileName));
@@ -21,45 +19,123 @@ public class ReadTextFile {
 			e.printStackTrace();
 		}
 	}
-
 	public void getMoves() {
-		String pattern = "(([\\s]*[K|Q|B|R|N]*[l|d]+[a-f]+[0-9]+)[-|x]*[a-f]*[0-9]*[+|#]*[\\s])*";
+		String pattern = "([\\s]*([PKQBRN]?)([ld]?)([a-h][1-8])([-x]?)([[a-h][1-8]]{2}|{0})([+#]?)[\\s]*)([\\s]*([PKQBRN]?)([ld]?)([a-h]?[1-8]?)([-x]?)([[a-h][1-8]]{2}|{0})([+#]?)[\\s]*)";
 		Pattern p = Pattern.compile(pattern);
 		Matcher m;
 		try {
-			while((line = read.readLine()) != null) {
-				m = p.matcher(line);		
+			while((line = read.readLine()) != null) {				
+				if(line.trim().matches("O-O")) {
+					System.out.println(line + " Black castle King's side.");
+				} else if(line.matches("O-O-O")) {
+					System.out.println(line + " White castle Queen's side.");
+				}
+				m = p.matcher(line);
 				boolean matches = m.find();	
 				if(matches) {
-					if(line.length() <= 4) {
-						System.out.println(line);
+					String piece = m.group(2);
+					String pieceName = null;
+					switch (piece) {
+					case "Q": pieceName = "Queen";
+					break;
+					case "K": pieceName = "King";
+					break;
+					case "R": pieceName = "Rook";
+					break;
+					case "": pieceName = "Pawn";
+					break;
+					case "N": pieceName = "Knight";
+					break;
+					case "B": pieceName = "Bishop";
+					break;
+					case "P": pieceName = "Pawn";
+					break;
 					}
+					String pieces = m.group(9);
+					String pieceName2 = null;
+					switch (pieces) {
+					case "Q": pieceName2 = "Queen";
+					break;
+					case "K": pieceName2 = "King";
+					break;
+					case "R": pieceName2 = "Rook";
+					break;
+					case "": pieceName2 = "Pawn";
+					break;
+					case "N": pieceName2 = "Knight";
+					break;
+					case "B": pieceName2 = "Bishop";
+					break;
+					case "P": pieceName2 = "Pawn";
+					break;
+					}
+					String piece2 = m.group(3);
+					String pieceColor = null;
+					switch (piece2) {
+					case "l": pieceColor = "White";
+					break;
+					case "d": pieceColor = "Black";
+					break;
+					}
+					String piece3 = m.group(5);
+					String pieceCaptured = null;
+					switch (piece3) {
+					case "-": pieceCaptured = "Move";
+					break;
+					case "x": pieceCaptured = "Captured";
+					break;
+					}
+					String piece4 = m.group(12);
+					String pieceCaptured2 = null;
+					switch (piece4) {
+					case "-": pieceCaptured2 = "Move";
+					break;
+					case "x": pieceCaptured2 = "Captured";
+					break;
+					}
+					String checkOrMate = null;
+					if(m.group(7).equals("+") || m.group(14).equals("+")) {
+						checkOrMate = "Check";
+					} else if(m.group(7).equals("#") || m.group(14).equals("#")) {
+						checkOrMate = "Check Mate";
+					}
+					if(line.startsWith("//")) {
+
+					} else if(m.group(10).isEmpty() && !m.group(8).isEmpty() || m.group(10).contains("d")) {
+						System.out.println(m.group(8) + " Black " + pieceName2 + " move from " + m.group(11) + " to " + m.group(13));
+//						if(pieceCaptured2.equals("Captured")) {
+//							System.out.println(m.group(8) + " " + pieceName2 + " move from " + m.group(11) + " to " + m.group(13) + " and Captured oponents piece ");
+
+//						}
+				    } else if(m.group(6).isEmpty() && line.length() > 5 ) {
+						System.out.println("Error: position out of range");
+					} else if(checkOrMate != null && pieceCaptured.equals("Captured") && pieceColor != null) {
+						System.out.println(m.group(0) + " " + pieceColor + " " + pieceName + " move from " + m.group(4) + " to " + m.group(6) + " and Captured oponents piece " + checkOrMate);
+					} else if(checkOrMate != null && pieceColor != null) {
+						System.out.println(m.group(0) + " " + pieceColor + " " + pieceName + " move from " + m.group(4) + " to " + m.group(6) + " " + checkOrMate);
+					} else if(!m.group(6).isEmpty() && pieceCaptured.equals("Captured") && pieceColor != null) {
+						System.out.println(m.group(0) + " " + pieceColor + " " + pieceName + " move from " + m.group(4) + " to " + m.group(6) + " and Captured oponents piece ");
+					} else if(!m.group(6).isEmpty() && pieceColor != null) {
+						System.out.println(m.group(0) + " " + pieceColor + " " + pieceName + " " + pieceCaptured + " from " + m.group(4) + " to " + m.group(6));
+					} else if(m.group(6).isEmpty() && pieceColor != null) {
+						System.out.println(m.group(0) + " " + pieceColor + " " + pieceName + " Position at " + m.group(4));
+					} else if(m.group(3).isEmpty()) {
+						if(checkOrMate != null && pieceCaptured.equals("Captured")) {
+							System.out.println(m.group(1).trim() + " White " + pieceName + " move from " + m.group(4) + " to " + m.group(6) + " and Captured oponents piece " + checkOrMate);
+						} else if(checkOrMate != null) {
+							System.out.println(m.group(1).trim() + " White " + pieceName + " move from " + m.group(4) + " to " + m.group(6) + " " + checkOrMate);
+						} else if(!m.group(6).isEmpty() && pieceCaptured.equals("Captured")) {
+							System.out.println(m.group(1).trim() + " White " + pieceName + " move from " + m.group(4) + " to " + m.group(6) + " and Captured oponents piece ");
+						} else if(!m.group(6).isEmpty()) {
+							System.out.println(m.group(1).trim() + " White " + pieceName + " " + pieceCaptured + " from " + m.group(4) + " to " + m.group(6));
+						} 
+					} else {
+						System.out.println("Unknown Error");
+					}	
 				}
-				//System.out.println("Moving from " + line.substring(0, 2) + " to " + line.substring(3, 5));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-	//	public void putPosition() {
-	//		String pattern = "[Q|K|B|R|N|C|O-O|O-O-O][l|d][a-f][0-9]";
-	//		pattern.toLowerCase();
-	//		Pattern p = Pattern.compile(pattern);
-	//		Matcher m;
-	//		try {			
-	//			while((line = read.readLine()) != null) {
-	//				line.toLowerCase();
-	//				m = p.matcher(line);		
-	//				boolean matches = m.find();	
-	//				if(matches) {
-	//					System.out.println(line);
-	//					//System.out.println("Put the queen at position " + line.substring(2, 4));
-	//				}
-	//			}
-	//
-	//		} catch (Exception e) {
-	//			e.printStackTrace();
-	//		}
-	//	}
 }
